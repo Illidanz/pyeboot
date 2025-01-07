9c273d9c6ad481d4c4a0bf506a0b36ad071a3dcd
Illidan <illidanrulez@gmail.com>
2025-01-07 15:11:41 +0100

    Build python 3.13 wheels.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ class build_ext_subclass(build_ext):
 
 def main():
     setup(name="pyeboot",
-          version="0.2.1",
+          version="0.2.2",
           author="Illidan",
           description="Python interface for pspdecrypt and sign_np.",
           long_description=long_description,
